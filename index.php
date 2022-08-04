a8647122e02d6c003a20e23efdb0c0a33917f7fe
Huy NguyenD <huy.nguyend@s3corp.com.vn>
2022-08-04 17:26:21 +0700

    function is ready: tomorrow : connect with Interface !

--- index.php
+++ index.php
@@ -3,8 +3,20 @@
 use Myapp\Data\Database;
 use Myapp\Student\Student;
 
-
 require_once realpath("vendor/autoload.php");
 
-$data  = new Database();
-$student = new Student();
+//$data  = new Database();
+$student = new Student(1, "Huy");
+$student->setMarks(100,1,70,4.5,5);
+echo $student->get_id(). "<br>";
+echo $student->get_name();
+echo $student->getMarks();
+
+echo "<br>Total Score: ". $student->totalScore();
+echo "<br>Average Score: ". $student->averageScore();
+
+echo "<br>Student Grade: ". $student->grade();
+echo "<br>Student final result: ". $student->finalResult();
+
+
+include "./index2.php";
--- src/Student/Student.php
+++ src/Student/Student.php
@@ -2,9 +2,97 @@
 
 namespace Myapp\Student;
 
-class Student{
-    public function __construct()
+class Student
+{
+    public $name;
+    public $id;
+    public $a, $b, $c, $d, $e;
+    public function __construct($id, $name)
     {
-        echo "made a student : ";
+        $this->name = $name;
+        $this->id = $id;
+    }
+    function get_id()
+    {
+        return $this->id;
+    }
+    function get_name()
+    {
+        return $this->name;
+    }
+    function setMarks(float $a, float $b, float $c, float $d, float $e)
+    {
+        //todo::
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+        $this->d = $d;
+        $this->e = $e;
+    }
+    function getMarks()
+    {
+        //todo::
+        echo "<br> A:" . $this->a;
+        echo " B:" . $this->b;
+        echo " C:" . $this->c;
+        echo " D:" . $this->d;
+        echo " E:" . $this->e;
+    }
+
+    function totalScore()
+    {
+        $aa = $this->a;
+        $bb = $this->b;
+        $cc = $this->c;
+        $dd = $this->d;
+        $ee = $this->e;
+        return $aa + $bb + $cc + $dd + $ee;
+    }
+
+    function averageScore()
+    {
+        return (self::totalScore()) / 5;
+    }
+
+    function grade()
+    {
+        $avg = $this->averageScore();
+        if ($avg > 70 && $avg <= 100) {
+            return "A";
+        }
+        if ($avg > 60 && $avg <= 69.9) {
+            return "B";
+        }
+        if ($avg > 50 && $avg <= 59.9) {
+            return "C";
+        }
+        if ($avg > 40 && $avg <= 49.9) {
+            return "D";
+        }
+        if ($avg > 0 && $avg <= 39.9) {
+            return "F";
+        }
+    }
+    function remark()
+    {
+        switch ($this->grade()) {
+            case "A":
+                return "Excellent";
+            case "B":
+                return "Very good";
+            case "C":
+                return "Good";
+            case "D":
+                return "Fair";
+            default:
+                return "Very poor";
+        }
+    }
+    function finalResult(){
+        if($this->averageScore()<40){
+            return "Fail";
+        }else{
+            return "Pass";
+        }
     }
 }
